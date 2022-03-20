66618cf95b5485fb62d2828887bd5e94f6046efb
Artyom Yudin <yudin.artyom@gmail.com>
2022-03-20 14:37:15 +0300

    move to ssl

--- app/server.js
+++ app/server.js
@@ -1,14 +1,53 @@
-const Stream = require('node-rtsp-stream');
+// const Stream = require('node-rtsp-stream');
+const https = require('https');
+const constants = require('crypto');
+const fs = require('fs');
+const Stream = require('./videoStream');
 
 const streamUrl = 'rtsp://video:123456@172.20.58.23:7070';
 const streamUrl1 = 'rtsp://video:123456@172.20.58.24:7070';
 const streamUrl2 = 'rtsp://video:123456@172.20.58.28:7070';
 
+const options = {
+  key: fs.readFileSync('./cert/center_inform.key'),
+  cert: fs.readFileSync('./cert/center_inform.crt'),
+  requestCert: false,
+  // secureProtocol: 'SSLv23_method',
+  secureOptions: constants.SSL_OP_NO_SSLv3 || constants.SSL_OP_NO_SSLv2,
+};
+
+const httpsServerMainCam = https
+  .createServer(options, (request, response) => {
+    if (request.method === 'OPTIONS') {
+      // response.writeHead(204, headers);
+      response.end();
+    }
+  })
+  .listen('9999', 'it.center-inform.ru', () => {});
+
+const httpsServerServerRoom1 = https
+  .createServer(options, (request, response) => {
+    if (request.method === 'OPTIONS') {
+      // response.writeHead(204, headers);
+      response.end();
+    }
+  })
+  .listen('9997', 'it.center-inform.ru', () => {});
+
+const httpsServerServerRoom2 = https
+  .createServer(options, (request, response) => {
+    if (request.method === 'OPTIONS') {
+      // response.writeHead(204, headers);
+      response.end();
+    }
+  })
+  .listen('9998', 'it.center-inform.ru', () => {});
+
 const stream = new Stream({
   name: 'ACTi_stream',
   streamUrl,
-  wsHost: '172.20.4.195',
-  wsPort: 9999,
+  wsHost: 'it.center-inform.ru',
+  wsServer: httpsServerMainCam,
   // width: 640,
   // height: 480,
   ffmpegOptions: {
@@ -27,8 +66,9 @@ const stream = new Stream({
 const stream1 = new Stream({
   name: 'ACTi_stream_SR1',
   streamUrl: streamUrl1,
-  wsHost: '172.20.4.195',
-  wsPort: 9997,
+  wsHost: 'it.center-inform.ru',
+  wsServer: httpsServerServerRoom1,
+  // wsPort: 9997,
   // width: 640,
   // height: 480,
   ffmpegOptions: {
@@ -47,10 +87,9 @@ const stream1 = new Stream({
 const stream2 = new Stream({
   name: 'ACTi_stream_SR2',
   streamUrl: streamUrl2,
-  wsHost: '172.20.4.195',
-  wsPort: 9998,
-  // width: 640,
-  // height: 480,
+  wsHost: 'it.center-inform.ru',
+  wsServer: httpsServerServerRoom2,
+  // wsPort: 9998,
   ffmpegOptions: {
     // '-stats': '', // an option with no neccessary value uses a blank string.
     '-f': 'mpegts',
